a0253d4565e0594b0086a7d17eaf0812c32e340e
Social Codia <socialcodia@gmail.com>
2020-05-15 13:59:27 +0530

    Added onClickListener at navigation

--- app/src/main/java/com/socialcodia/studentmanagementsystem/activity/MainActivity.java
+++ app/src/main/java/com/socialcodia/studentmanagementsystem/activity/MainActivity.java
@@ -1,16 +1,20 @@
 package com.socialcodia.studentmanagementsystem.activity;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Patterns;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.socialcodia.studentmanagementsystem.R;
 import com.socialcodia.studentmanagementsystem.model.UserModel;
 import com.socialcodia.studentmanagementsystem.storage.SharedPrefManager;
@@ -21,13 +25,17 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView username;
     SharedPrefManager sharedPrefManager;
+    BottomNavigationView navigationView;
+    ActionBar actionBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         username = findViewById(R.id.username);
+        navigationView = findViewById(R.id.bottomNavigation);
 
+        actionBar = getSupportActionBar();
 
         UserModel user = SharedPrefManager.getInstance(getApplicationContext()).getUser();
 
@@ -38,6 +46,25 @@ public class MainActivity extends AppCompatActivity {
             sendToLogin();
         }
 
+        navigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                switch (id)
+                {
+                    case R.id.miHome:
+                        actionBar.setTitle("Home");
+                        break;
+                    case R.id.miUsers:
+                        actionBar.setTitle("Users");
+                        break;
+                    case R.id.miSettings:
+                        actionBar.setTitle("Settings");
+                        break;
+                }
+                return true;
+            }
+        });
 
     }
 
